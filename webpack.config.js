b257ca0da4d1138f948c1c942084d032a7314c87
Tomasz Adrianowski <tomasz.adrianowski@metro-external.digital>
2024-04-18 11:32:34 +0200

    Add Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    publicPath: "auto",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    }),
+    new ModuleFederationPlugin({
+      name: 'MyReactComponent',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Component': './src/Component',
+      },
+      shared: {
+        react: { singleton: true, eager: true },
+        "react-dom": { singleton: true, eager: true }
+      },
+    }),
+  ],
+};
