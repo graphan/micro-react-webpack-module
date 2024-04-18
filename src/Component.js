384a04d0dfd24daa8d7147f4143f76bd19e6b105
Tomasz Adrianowski <tomasz.adrianowski@metro-external.digital>
2024-04-18 11:28:53 +0200

    Add React Component and index.html

---
+++ src/Component.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MyComponent = () => (
+  <div>
+    <h1>Hello from My React Component</h1>
+  </div>
+);
+
+export default MyComponent;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MyComponent from './Component';
+
+ReactDOM.render(<MyComponent />, document.getElementById('app'));
